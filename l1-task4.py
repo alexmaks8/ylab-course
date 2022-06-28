d2b78c87ef5e377102990756bd456acee543c82e
Alexander M <106433075+alexmaks8@users.noreply.github.com>
2022-06-28 12:02:16 +0300

    Update l1-task4.py

--- l1-task4.py
+++ l1-task4.py
@@ -1,3 +1,29 @@
+'''
+Задача №4. Секция статьи "Задача №4."
+Написать метод bananas, который принимает на вход строку и
+возвращает количество слов «banana» в строке.
+
+(Используйте - для обозначения зачеркнутой буквы)
+
+Input: bbananana
+
+Output:
+
+b-anana--
+b-anan--a
+b-ana--na
+b-an--ana
+b-a--nana
+b---anana
+-banana--
+-banan--a
+-bana--na
+-ban--ana
+-ba--nana
+-b--anana
+'''
+
+
 from itertools import combinations
 
 def bananas(s):
